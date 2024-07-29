4c6ad6adba311656532956c6c6bf53d96449162f
darcy <acednes@gmail.com>
2024-07-29 21:54:25 +1000

    text color

--- config.h
+++ config.h
@@ -123,7 +123,7 @@ static const char *colorname[] = {
 	/* more colors can be added after 255 to use with DefaultXX */
 	"#cccccc",
 	"#555555",
-	"gray90", /* default foreground colour */
+	"#dddddd", /* default foreground colour */
 	"#080b10", /* default background colour */
 };
 
